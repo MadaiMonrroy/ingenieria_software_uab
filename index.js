095c8ca03be691ab03b029012bcc6aa01b4b636c
MadaiMonrroy <madaimonrroy2810@gmail.com>
2026-04-23 13:05:28 -0400

    trabajopooterminado

---
+++ index.js
@@ -0,0 +1,69 @@
+class Perro {
+  constructor(nombre, raza, edad) {
+    this.nombre = nombre;
+    this.raza   = raza;
+    this.edad   = edad;
+  }
+
+  presentarse() {
+    console.log(`Hola, soy ${this.nombre}, un ${this.raza} y tengo ${this.edad} años`);
+  }
+
+  ladrar() {
+    console.log(`${this.nombre} dice: ¡Guau!`);
+  }
+
+  cumplirAnios() {
+    this.edad++;
+    console.log(`¡Felicidades ${this.nombre}! Ahora tienes ${this.edad} años`);
+  }
+}
+
+
+class PerroGuia extends Perro {
+  constructor(nombre, edad, dueno) {
+    super(nombre, 'Labrador', edad);
+    this.dueno = dueno;
+  }
+
+  ladrar() {
+    console.log(`${this.nombre} ladra suavemente... cuida a ${this.dueno}`);
+  }
+
+  guiar() {
+    console.log(`${this.nombre} está guiando a ${this.dueno} con cuidado`);
+  }
+}
+
+class PerroPolicial extends Perro {
+  constructor(nombre, edad, rango) {
+    super(nombre, 'Pastor Alemán', edad);
+    this.rango = rango;
+  }
+
+  ladrar() {
+    console.log(`${this.nombre} [${this.rango}]: ¡GUAU GUAU! ¡Alto ahí!`);
+  }
+
+  patrullar() {
+    console.log(`${this.nombre} está patrullando el área`);
+  }
+}
+
+const bobby = new Perro('Bobby', 'Salchicha', 3);
+const luna  = new PerroGuia('Luna', 4, 'Don Carlos');
+const rex   = new PerroPolicial('Rex', 6, 'Sargento');
+
+
+bobby.presentarse();
+bobby.ladrar();   // polimorfismo
+bobby.cumplirAnios();
+
+luna.presentarse();
+luna.ladrar();      // polimorfismo
+luna.guiar();
+
+rex.presentarse();
+rex.ladrar();   // polimorfismo
+rex.patrullar();
+
